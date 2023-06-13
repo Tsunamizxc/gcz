5f513cbbf42cfccfd99de68651ce250e1d8edc44
Tsunamizxc <semen.gnedenkov@mail.ru>
2023-06-13 23:00:36 +0600

    add contacts 13 06 2023

--- js/script.js
+++ js/script.js
@@ -1,57 +1,74 @@
 
 $('.slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    arrows: false,
-    autoplaySpeed: 2000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows: false,
+  autoplaySpeed: 2000,
 });
 
 $(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() != 0) {
-            $('#topNubex').fadeIn();
-        } else {
-            $('#topNubex').fadeOut();
-        }
-    });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() != 0) {
+      $('#topNubex').fadeIn();
+    } else {
+      $('#topNubex').fadeOut();
+    }
+  });
 });
 
 
 $(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() != 0) {
-            $('#topNubexTel').fadeIn();
-        } else {
-            $('#topNubexTel').fadeOut();
-        }
-    });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() != 0) {
+      $('#topNubexTel').fadeIn();
+    } else {
+      $('#topNubexTel').fadeOut();
+    }
+  });
 });
 
 
 
 $('.menu').click(function () {
-    $(this).toggleClass('open');
+  $(this).toggleClass('open');
 });
 
 let openp = document.getElementById('openp');
+let openc = document.getElementById('openc');
 let popup = document.querySelector('.popup-vis');
 let exitp = document.querySelector('.popup-exit');
 let bg = document.querySelector('.bg')
 
 
 openp.addEventListener("click", function () {
-    popup.style.display = 'block';
-    bg.style.display = 'block';
+  popup.style.display = 'block';
+  bg.style.display = 'block';
 });
 bg.addEventListener("click", function () {
-    popup.style.display = 'none';
-    bg.style.display = 'none';
+  popup.style.display = 'none';
+  bg.style.display = 'none';
 });
 exitp.addEventListener("click", function () {
-    popup.style.display = 'none';
-    bg.style.display = 'none';
+  popup.style.display = 'none';
+  bg.style.display = 'none';
 });
+
+
+openc.addEventListener("click", function (e) {
+  e.preventDefault();
+  popup.style.display = 'block';
+  bg.style.display = 'block';
+});
+bg.addEventListener("click", function () {
+  popup.style.display = 'none';
+  bg.style.display = 'none';
+});
+exitp.addEventListener("click", function () {
+  popup.style.display = 'none';
+  bg.style.display = 'none';
+});
+
 
 
 
